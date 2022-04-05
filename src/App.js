9edf8d1a2d1d397473b0f05ddc0ca3a8e4037b31
Cits123 <101889069+Cits123@users.noreply.github.com>
2022-04-05 09:26:56 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Navbar from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+//import Teams from './pages/team';
+import Blogs from './pages/blog';
+import SignUp from './pages/signup';
+import Login from './pages/login';
+import Products from './pages/product';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Switch>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+		<Route path='/events' component={Events} />
+		<Route path='/annual' component={AnnualReport} />
+		{/* <Route path='/team' component={Teams} /> */}
+		<Route path='/blog' component={Blogs} />
+		<Route path='/signup' component={SignUp} />
+    <Route path='/login' component={Login} />
+	<Route path='/products' component ={Products}></Route>
+	</Switch>
+	<footer>
+	
+       
+	2022 Copy Rights @ CITS
+  
+
+</footer>
+	</Router>
+);
+}
+
+export default App;
